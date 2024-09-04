21b7725aaf28a987cc4dbbd23f87cde091be9e84
chienhow29 <chienhow29@1utar.my>
2024-09-04 20:52:42 +0800

    Add files via upload

---
+++ src/test/java/my/edu/utar/TestSuite.java
@@ -0,0 +1,41 @@
+<<<<<<< HEAD
+package my.edu.utar;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(value = Suite.class)
+@SuiteClasses(value = { 
+		BookingTest.class,
+		UserTest.class, 
+		WaitingListTest.class, 
+		BookingIntegrationTest.class,
+		WaitingListIntegrationTest.class,
+		})
+
+public class TestSuite {
+}
+
+
+=======
+package my.edu.utar;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(value = Suite.class)
+@SuiteClasses(value = { 
+		BookingTest.class,
+		UserTest.class, 
+		WaitingListTest.class, 
+		BookingIntegrationTest.class,
+		WaitingListIntegrationTest.class,
+		})
+
+public class TestSuite {
+}
+
+
+>>>>>>> 4b7315afe00ec31d1241e9822c6a40f994f1823f
